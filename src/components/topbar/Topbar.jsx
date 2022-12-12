6f35d7c10bbf637fc13889712d795f87df7d0026
onur <onur.un0034@gmail.com>
2022-12-12 17:14:46 +0300

    topbar left

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,7 +1,27 @@
 import "./topbar.scss";
+import PersonIcon from "@mui/icons-material/Person";
+import EmailIcon from "@mui/icons-material/Email";
 
 export default function Topbar() {
   return (
-    <div className='topbar'></div>
-  )
+    <div className="topbar" id="topbar">
+      <div className="wrapper">
+        <div className="left">
+          <a href="#intro" className="logo">
+            genius.
+          </a>
+          <div className="itemContainer">
+            <PersonIcon className="icon" />
+            <span>+90 5455602139</span>
+          </div>
+          <div className="itemContainer">
+            <EmailIcon className="icon" />
+            <span>onur.un0034@gmail.com</span>
+          </div>
+        </div>
+
+        <div className="right"></div>
+      </div>
+    </div>
+  );
 }
