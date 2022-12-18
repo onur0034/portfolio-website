3b406a7ae27e143639192e19aaf046b2cc4068ab
onur <onur.un0034@gmail.com>
2022-12-18 13:13:36 +0300

    intro page

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ import Testimonials from "./components/testimonials/Testimonials";
 import Contact from "./components/contact/Contact";
 import "./app.scss";
 import { useState } from "react";
-import { Menu } from "@mui/material";
+import Menu from "./components/menu/Menu";
 
 function App() {
   const [menuOpen, setMenuOpen] = useState(false);
   return (
     <div className="app">
       <Topbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-      <Menu />
+      <Menu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
       <div className="sections">
         <Intro />
         <Portfolio />
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,5 +1,27 @@
 import "./intro.scss";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 export default function Intro() {
-  return <div className="intro" id="intro"></div>;
+  return (
+    <div className="intro" id="intro">
+      <div className="left">
+        <div className="imgContainer">
+          <img src="assets/portfolio.png" alt="" />
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>Hi Guys!</h2>
+          <h1>I am Onur Un</h1>
+          <h3>Frontend Developer</h3>
+        </div>
+        <a href="#portfolio">
+          <KeyboardArrowDownIcon
+            className="keyboardArrowDownIcon"
+            fontSize="large"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
