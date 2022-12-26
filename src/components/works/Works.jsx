c97eec9080e736409df6c9f250871b454e78ebe0
onur <onur.un0034@gmail.com>
2022-12-26 15:03:14 +0300

    works slider

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,5 +1,77 @@
+import { useState } from "react";
 import "./works.scss";
 
+
 export default function Works() {
-  return <div className="works" id="works"></div>;
+  const [ currentSlide, setCurrentSlide ] = useState(0);
+  const data = [
+    {
+      id: 1,
+      icon: "https://w7.pngwing.com/pngs/588/140/png-transparent-mobile-app-euclidean-icon-click-the-ppt-phone-material-blue-gadget-hand.png",
+      title: "Mobile Application",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      img: "assets/imagination.jpeg",
+    },
+    {
+      id: 2,
+      icon: "https://flyclipart.com/thumb2/github-png-icon-free-download-813498.png",
+      title: "Github Profile",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      img: "assets/imagination.jpeg",
+    },
+    {
+      id: 3,
+      icon: "https://img1.pngindir.com/20180406/hpw/kisspng-linkedin-logo-computer-icons-comcast-business-get-started-now-button-5ac6f543cb6bf0.2094725315229883558332.jpg",
+      title: "LinkedIn Profile",
+      desc: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
+      img: "assets/imagination.jpeg",
+    },
+  ];
+
+  const handleClick = (way) => {
+    console.log("clicked");
+    way === "left"
+      ? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
+      : setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+  return (
+    <div className="works" id="works">
+      <div
+        className="slider"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="left-container">
+                  <div className="img-container">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img src={d.img} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img
+        src="assets/arrow.png"
+        alt=""
+        className="arrow left"
+        onClick={() => handleClick("left")}
+      />
+      <img
+        src="assets/arrow.png"
+        alt=""
+        className="arrow right"
+        onClick={() => handleClick()}
+      />
+    </div>
+  );
 }
--- src/data.js
+++ src/data.js
@@ -35,97 +35,97 @@ export const webPortfolio = [
   {
     id: 1,
     title: "E-Shop",
-    img: "https://w7.pngwing.com/pngs/385/345/png-transparent-web-development-mobile-app-development-software-development-web-application-development-web-application-development-web-design-text-web-application.png",
+    img: "https://www.shutterstock.com/image-vector/concept-web-design-website-page-260nw-1939082209.jpg",
   },
   {
     id: 2,
     title: "E-Shop",
-    img: "https://w7.pngwing.com/pngs/385/345/png-transparent-web-development-mobile-app-development-software-development-web-application-development-web-application-development-web-design-text-web-application.png",
+    img: "https://www.shutterstock.com/image-vector/concept-web-design-website-page-260nw-1939082209.jpg",
   },
   {
     id: 3,
     title: "E-Shop",
-    img: "https://w7.pngwing.com/pngs/385/345/png-transparent-web-development-mobile-app-development-software-development-web-application-development-web-application-development-web-design-text-web-application.png",
+    img: "https://www.shutterstock.com/image-vector/concept-web-design-website-page-260nw-1939082209.jpg",
   },
   {
     id: 4,
     title: "E-Shop",
-    img: "https://w7.pngwing.com/pngs/385/345/png-transparent-web-development-mobile-app-development-software-development-web-application-development-web-application-development-web-design-text-web-application.png",
+    img: "https://www.shutterstock.com/image-vector/concept-web-design-website-page-260nw-1939082209.jpg",
   },
   {
     id: 5,
     title: "E-Shop",
-    img: "https://w7.pngwing.com/pngs/385/345/png-transparent-web-development-mobile-app-development-software-development-web-application-development-web-application-development-web-design-text-web-application.png",
+    img: "https://www.shutterstock.com/image-vector/concept-web-design-website-page-260nw-1939082209.jpg",
   },
   {
     id: 6,
     title: "E-Shop",
-    img: "https://w7.pngwing.com/pngs/385/345/png-transparent-web-development-mobile-app-development-software-development-web-application-development-web-application-development-web-design-text-web-application.png",
+    img: "https://www.shutterstock.com/image-vector/concept-web-design-website-page-260nw-1939082209.jpg",
   },
 ];
 
 export const mobilePortfolio = [
   {
     id: 1,
-    title: "mobile App",
-    img: "https://www.pngitem.com/pimgs/m/294-2945902_market-your-mobile-app-on-social-media-png.png",
+    title: "Mobile App",
+    img: "https://img.etimg.com/thumb/msid-46065504,width-1200,height-900,imgsize-52163,overlay-economictimes/photo.jpg",
   },
   {
     id: 2,
-    title: "mobile App",
-    img: "https://www.pngitem.com/pimgs/m/294-2945902_market-your-mobile-app-on-social-media-png.png",
+    title: "Mobile App",
+    img: "https://img.etimg.com/thumb/msid-46065504,width-1200,height-900,imgsize-52163,overlay-economictimes/photo.jpg",
   },
   {
     id: 3,
-    title: "mobile App",
-    img: "https://www.pngitem.com/pimgs/m/294-2945902_market-your-mobile-app-on-social-media-png.png",
+    title: "Mobile App",
+    img: "https://img.etimg.com/thumb/msid-46065504,width-1200,height-900,imgsize-52163,overlay-economictimes/photo.jpg",
   },
   {
     id: 4,
-    title: "mobile App",
-    img: "https://www.pngitem.com/pimgs/m/294-2945902_market-your-mobile-app-on-social-media-png.png",
+    title: "Mobile App",
+    img: "https://img.etimg.com/thumb/msid-46065504,width-1200,height-900,imgsize-52163,overlay-economictimes/photo.jpg",
   },
   {
     id: 5,
-    title: "mobile App",
-    img: "https://www.pngitem.com/pimgs/m/294-2945902_market-your-mobile-app-on-social-media-png.png",
+    title: "Mobile App",
+    img: "https://img.etimg.com/thumb/msid-46065504,width-1200,height-900,imgsize-52163,overlay-economictimes/photo.jpg",
   },
   {
     id: 6,
-    title: "mobile App",
-    img: "https://www.pngitem.com/pimgs/m/294-2945902_market-your-mobile-app-on-social-media-png.png",
+    title: "Mobile App",
+    img: "https://img.etimg.com/thumb/msid-46065504,width-1200,height-900,imgsize-52163,overlay-economictimes/photo.jpg",
   },
 ];
 
 export const designPortfolio = [
   {
     id: 1,
-    title: "design App",
+    title: "Design App",
     img: "https://png.pngtree.com/png-vector/20200312/ourmid/pngtree-modern-flat-design-concept-of-freelancer-blogger-content-creator-copy-writer-png-image_2157848.jpg",
   },
   {
     id: 2,
-    title: "design App",
+    title: "Design App",
     img: "https://png.pngtree.com/png-vector/20200312/ourmid/pngtree-modern-flat-design-concept-of-freelancer-blogger-content-creator-copy-writer-png-image_2157848.jpg",
   },
   {
     id: 3,
-    title: "design App",
+    title: "Design App",
     img: "https://png.pngtree.com/png-vector/20200312/ourmid/pngtree-modern-flat-design-concept-of-freelancer-blogger-content-creator-copy-writer-png-image_2157848.jpg",
   },
   {
     id: 4,
-    title: "design App",
+    title: "Design App",
     img: "https://png.pngtree.com/png-vector/20200312/ourmid/pngtree-modern-flat-design-concept-of-freelancer-blogger-content-creator-copy-writer-png-image_2157848.jpg",
   },
   {
     id: 5,
-    title: "design App",
+    title: "Design App",
     img: "https://png.pngtree.com/png-vector/20200312/ourmid/pngtree-modern-flat-design-concept-of-freelancer-blogger-content-creator-copy-writer-png-image_2157848.jpg",
   },
   {
     id: 6,
-    title: "design App",
+    title: "Design App",
     img: "https://png.pngtree.com/png-vector/20200312/ourmid/pngtree-modern-flat-design-concept-of-freelancer-blogger-content-creator-copy-writer-png-image_2157848.jpg",
   },
 ];
@@ -133,32 +133,32 @@ export const designPortfolio = [
 export const contentPortfolio = [
   {
     id: 1,
-    title: "content App",
+    title: "Content App",
     img: "https://img.lovepik.com/element/45007/4831.png_300.png",
   },
   {
     id: 2,
-    title: "content App",
+    title: "Content App",
     img: "https://img.lovepik.com/element/45007/4831.png_300.png",
   },
   {
     id: 3,
-    title: "content App",
+    title: "Content App",
     img: "https://img.lovepik.com/element/45007/4831.png_300.png",
   },
   {
     id: 4,
-    title: "content App",
+    title: "Content App",
     img: "https://img.lovepik.com/element/45007/4831.png_300.png",
   },
   {
     id: 5,
-    title: "content App",
+    title: "Content App",
     img: "https://img.lovepik.com/element/45007/4831.png_300.png",
   },
   {
     id: 6,
-    title: "content App",
+    title: "Content App",
     img: "https://img.lovepik.com/element/45007/4831.png_300.png",
   },
 ];
