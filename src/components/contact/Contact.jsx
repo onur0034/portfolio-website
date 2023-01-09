16065548073b33c25a056dba641940f61a51f5b3
onur <onur.un0034@gmail.com>
2023-01-09 22:27:03 +0300

    contact part

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,32 @@
+import { useState } from "react";
 import "./contact.scss";
 
 export default function Contact() {
-  return <div className="contact" id="contact"></div>;
+  const [message, setMessage] = useState(false);
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+    setMessage(true);
+  }
+  return (
+    <div className="contact" id="contact">
+      <div className="left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/2652/2652673.png"
+          alt=""
+        />
+      </div>
+      <div className="right">
+        <h2>contact.</h2>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="email" />
+
+          <textarea placeholder="message"></textarea>
+
+          <button type="submit">send</button>
+          {message && <span>thx for opinions!</span>}
+        </form>
+      </div>
+    </div>
+  );
 }
